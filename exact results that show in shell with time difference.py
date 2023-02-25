103d8850fa5866fa005ea1a12272274bdcd4d2b7
ChaitrikaBiswal <125036934+ChaitrikaBiswal@users.noreply.github.com>
2023-02-25 11:24:10 +0530

    Add files via upload

---
+++ exact results that show in shell with time difference.py
@@ -0,0 +1,8 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\chait\OneDrive - Vignan's Institute of Management & Technology for Women\Desktop\check intern code\server3.py
+HTTP server started on port 8080
+Query received @ t2019-02-10 10:07:43.237974
+Query received @ t2019-02-11 18:12:31.763344
+Query received @ t2019-02-13 00:41:03.061658
